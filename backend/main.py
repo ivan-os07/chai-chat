bd182fd66f822b127fe6acd8b3e117dbc278a41c
Ivan Chizhov <ivan07-os@yandex.ru>
2024-12-15 22:46:08 +0500

    first commit

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello():
+    return {"hello": "chAI-chat"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
