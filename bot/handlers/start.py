2e10614788104807f7b98b06cc89d5a6e9ab61f5
Ivan Chizhov <ivan07-os@yandex.ru>
2025-01-05 22:45:33 +0500

    remove backend nahyu

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -28,22 +28,3 @@ async def command_start_handler(message: Message) -> None:
         reply_markup=can_kb,
     )
 
-
-# Пример как будет посылаться запрос к беку
-@router.message(F.text == "/test")
-async def test(message: Message):
-    # Ваш JWT токен
-    jwt_token = "eyJhbGJ9.eyJzdWIiOiJ0ZXN0IiwianRpIjoiYTlmOTNmM2QtMDJmZC00NDhhLTkwMDctODlmYjk1ZjIxYWY3IiwidHlwZSI6ImFjY2VzcyIsImZyZXNoIjpmYWxzZSwiY3NyZiI6IiIsImlhdCI6MTczNTY1ODMyNSwiZXhwIjoxNzM1NjU5MjI1LjMwMzY3Nn0.QeBU_8oHGAaPkvBt6cF6PO_u3pUGvO_jOXVF3U5ZbLU"
-
-    # URL, к которому вы хотите отправить запрос
-    url = "http://127.0.0.1:8000/api/v1/protected/"
-
-    # Заголовки запроса
-    headers = {
-        "Authorization": f"Bearer {jwt_token}",
-    }
-
-    # Пример GET запроса
-    response = requests.get(url, headers=headers)
-
-    await message.answer(f"{response.json()}")
